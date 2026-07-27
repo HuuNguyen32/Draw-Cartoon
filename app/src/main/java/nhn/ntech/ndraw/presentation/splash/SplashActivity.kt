ca991ca353d4f7127ff778dafcdf5c00e23649a3
HuuNguyen32 <nhn322004@gmail.com>
2026-07-27 17:29:19 +0700

    Remove unnecessary imports

--- app/src/main/java/nhn/ntech/ndraw/presentation/splash/SplashActivity.kt
+++ app/src/main/java/nhn/ntech/ndraw/presentation/splash/SplashActivity.kt
@@ -3,7 +3,6 @@ package nhn.ntech.ndraw.presentation.splash
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.ViewModelProvider
